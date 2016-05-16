3d2d64f0b603b8d9965f6434312e6c447a4706fe
Doolly <treeland88@hotmail.com>
2016-05-17 03:59:18 +0900

    명상이 왔다간후
    시리얼 딜레이 삭제
    atan2로 수식변화
    플래그 다운으로 dif값 '조금' 낮아짐

--- step_test/functions.ino
+++ step_test/functions.ino
@@ -1,96 +1,82 @@
-double Get_angle(void) {
-  while ((flag_now[0] == false) || (flag_now[1] == false) || (flag_now[2] == false));
-  time_dif[1] = stamp_now[1] - stamp_now[0];
-  time_dif[2] = stamp_now[2] - stamp_now[0];
-  Serial.print("time_dif[1] = "); Serial.println(time_dif[1]);
-  Serial.print("time_dif[2] = "); Serial.println(time_dif[2]);
-  double tangential = 1.15 * time_dif[2] / time_dif[1] - 0.58;
-  if (tangential > 0 ) {
-    if (time_dif[2] < 0) {
-      angle = atan(tangential);
-      Serial.print("angle 1sabun = "); Serial.println(angle);
+void Get_angle(void) {
+  if (flag_now[0] == true && flag_now[1] == true && flag_now[2] == true) {
+    time_dif[1] = stamp_now[1] - stamp_now[0];
+    time_dif[2] = stamp_now[2] - stamp_now[0];
+    Serial.print("time_dif[1] = "); Serial.println(time_dif[1]);
+    Serial.print("time_dif[2] = "); Serial.println(time_dif[2]);
+
+    if (abs(time_dif[1]) < 1200 && abs(time_dif[2]) < 1200) {
+      double tangential = (-60 * time_dif[2] + 300 * time_dif[1]) / -52 * time_dif[1] ;
+      angle = atan2((-60 * time_dif[2] + 300 * time_dif[1]), -52 * time_dif[1]);
+
+      double angle_in_degree = angle * 180 / PI;
+
+      Serial.print("tangential = "); Serial.println(tangential);
+      Serial.print("angle = "); Serial.print(angle);
+      Serial.print("    angle_in_degree = "); Serial.println(angle_in_degree);
     }
     else {
-      angle = atan(tangential) - PI / 2;
-      Serial.print("angle 3sabun = "); Serial.println(angle);
+      Serial.println("Error!! ");
     }
+    flag_now[0] = false;
+    flag_now[1] = false;
+    flag_now[2] = false;
   }
-  else  {
-    if ((stamp_now[2] > stamp_now[0]) && (stamp_now[2] > stamp_now[1])) {
-      angle = atan(tangential);
-      Serial.print("angle 2sabun = "); Serial.println(angle);
+  else if (!(flag_now[0] == flag_now[1] == flag_now[2] == 0) && (flag_now[0] == false || flag_now[1] == false || flag_now[2] == false)) {
+    if (micros() - main_stamp > 1500) {
+      Serial.print("reset flags\n");
+      flag_now[0] = false;
+      flag_now[1] = false;
+      flag_now[2] = false;
+      main_stamp = micros();
     }
-    else {
-      angle = atan(tangential) + PI / 2;
-      Serial.print("angle 4sabun = "); Serial.println(angle);
-    }
+
   }
-  double angle_in_degree = angle * 180 / PI;
-  Serial.print("angle = ");
-  Serial.print(angle);
-  Serial.print("    angle_in_degree = ");
-  Serial.println(angle_in_degree);
-
-  flag_now[0] = false;
-  flag_now[1] = false;
-  flag_now[2] = false;
-
-  return angle;
 }
-
 void Move(int Angle_now) {
-  // int angle_dif = Angle_now - last_angle;
-  //if (angle_dif != 0) {
-  int num_step = map(Angle_now, -PI, PI, -200, 200);
-  stepper.step(num_step);
-  last_angle = Angle_now;
-  PhaseFree();
-  Serial.print("num_step = ");
-  Serial.println(num_step);
-  Serial.println("/////////////////////////////////////////////////////////////////////////////");
-  // }
+  int angle_dif = Angle_now - last_angle;
+  if (angle_dif != 0) {
+    int num_step = map(Angle_now, -PI, PI, -100, 100);
+    stepper.step(num_step);
+    last_angle = Angle_now;
+    PhaseFree();
+    Serial.print("num_step = ");
+    Serial.println(num_step);
+    Serial.println("/////////////////////////////////////////////////////////////////////////////");
+  }
 }
 
 void Mic_0_stamp(void) {
   unsigned long temp;
   temp = micros();
+  main_stamp = temp;
   if (((temp - stamp_last[0]) > bounce_delay)) {
     stamp_now[0] = temp;
     flag_now[0] = true;
   }
   stamp_last[0] = stamp_now[0];
-  Serial.print("Mic_0 stamp = ");
-  Serial.print(stamp_now[0]);
-  Serial.print("   flag_now[0] = ");
-  Serial.println(flag_now[0]);
 }
 
 void Mic_1_stamp(void) {
   unsigned long temp;
   temp = micros();
+  main_stamp = temp;
   if (((temp - stamp_last[1]) > bounce_delay)) {
     stamp_now[1] = temp;
     flag_now[1] = true;
   }
   stamp_last[1] = stamp_now[1];
-  Serial.print("Mic_1 stamp = ");
-  Serial.print(stamp_now[1]);
-  Serial.print("   flag_now[1] = ");
-  Serial.println(flag_now[1]);
 }
 
 void Mic_2_stamp(void) {
   unsigned long temp;
   temp = micros();
+  main_stamp = temp;
   if (((temp - stamp_last[2]) > bounce_delay)) {
     stamp_now[2] = temp;
     flag_now[2] = true;
   }
   stamp_last[2] = stamp_now[2];
-  Serial.print("Mic_2 stamp = ");
-  Serial.print(stamp_now[2]);
-  Serial.print("   flag_now[2] = ");
-  Serial.println(flag_now[2]);
 }
 
 void PhaseFree(void) {
