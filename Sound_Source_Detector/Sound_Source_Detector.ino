30d3a42c607f734e461af37e03d90552e476b412
Doolly <treeland88@hotmail.com>
2018-02-27 06:11:20 +0900

    dd
    ㅇ

--- Sound_Source_Detector/Sound_Source_Detector.ino
+++ Sound_Source_Detector/Sound_Source_Detector.ino
@@ -1,15 +1,24 @@
 #include <Stepper.h>
 #include <math.h>
-#define STEPS 200 //모터의 스텝수
-#define MIC_0 2
-#define MIC_1 4
-#define MIC_2 6
-#define MOTER_PIN_1 8
-#define MOTER_PIN_2 9
-#define MOTER_PIN_3 10
-#define MOTER_PIN_4 11
+#include <Servo.h>
 
-Stepper stepper(STEPS, MOTER_PIN_1, MOTER_PIN_2, MOTER_PIN_3, MOTER_PIN_4);
+#define STEPS 200 //모터의 스텝수
+#define MIC_0 14
+#define MIC_1 15
+#define MIC_2 16
+#define MIC_3 17
+//#define STEP_PIN_1 3
+//#define STEP_PIN_2 4
+//#define STEP_PIN_3 5
+//#define STEP_PIN_4 6
+#define SERVO_PIN 7
+#define stp 2
+#define dir 3
+#define MS1 4
+#define MS2 5
+#define EN  6
+
+Servo servo;
 
 void Change_Value_in_Serial(void);
 void PhaseFree(void);
@@ -22,27 +31,37 @@ String command; //debuging 용 변수
 double angle = 0;
 int angle_step = 0;
 int last_angle_step = 0;
-long time_dif[3] = {0, 0, 0};
-//time_dif[0] = 발생지에서 2번 까지 걸린 시간 - 1번 까지 걸린 시간
-//time_dif[1] = 발생지에서 1번 까지 걸린 시간 - 0번 까지 걸린 시간
-//time_dif[2] = 발생지에서 2번 까지 걸린 시간 - 0번 까지 걸린 시간
-bool flag_now[3] = {false, false, false};
+
+long time_dif[4] = {0, 0, 0, 0};
+bool flag_now[4] = {false, false, false, false};
 
 void setup() {
   pinMode(MIC_0, INPUT);
   pinMode(MIC_1, INPUT);
   pinMode(MIC_2, INPUT);
+  pinMode(MIC_3, INPUT);
+
+  pinMode(stp, OUTPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(EN, OUTPUT);
   Serial.begin(9600);
-  stepper.setSpeed(moter_speed);
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
   TimeStamp();
   GetAngle();
-  Move();
+  //  Move();
+  //Change_Value_in_Serial();
+  // PhaseFree();
+  //motorctrl(100, angle_step)
+
+
 }
 
-//Change_Value_in_Serial();
+
 
 
 
--- Sound_Source_Detector/functions.ino
+++ Sound_Source_Detector/functions.ino
@@ -1,18 +1,26 @@
 void TimeStamp(void) {
-  int loop_elapse = 13; //while 문 처리시간
-  for (int i = 0 ; i < 3; i++) {
+  int loop_elapse = 20; //while 문 처리시간 micro sec
+  for (int i = 0 ; i < 4; i++) {
     time_dif[i] = 0;
     flag_now[i] = false;
   }
-  if ((digitalRead(MIC_0) == HIGH ) | ( digitalRead(MIC_1) == HIGH ) | (digitalRead(MIC_2) == HIGH)) { //먼저 하나 받으면 while 진입
+  if ((digitalRead(MIC_0) == HIGH ) | ( digitalRead(MIC_1) == HIGH ) | (digitalRead(MIC_2) == HIGH) | (digitalRead(MIC_3) == HIGH)) { //먼저 하나 받으면 while 진입
 
-    while ((flag_now[0] + flag_now[1] + flag_now[2]) < 3) {
+    //time_dif[1] = 발생지에서 1번 까지 걸린 시간 - 0번 까지 걸린 시간
+    //time_dif[2] = 발생지에서 2번 까지 걸린 시간 - 0번 까지 걸린 시간
+    //time_dif[3] = 발생지에서 3번 까지 걸린 시간 - 0번 까지 걸린 시간
+
+    while ((flag_now[0] + flag_now[1] + flag_now[2] + flag_now[3]) < 4) {
+
       if (digitalRead(MIC_0) == HIGH) {
         if (digitalRead(MIC_1) == LOW) {
           time_dif[1] += loop_elapse;
         }
         if (digitalRead(MIC_2) == LOW) {
           time_dif[2] += loop_elapse;
+        }
+        if (digitalRead(MIC_3) == LOW) {
+          time_dif[3] += loop_elapse;
         }
         flag_now[0] = true;
       }
@@ -21,9 +29,6 @@ void TimeStamp(void) {
         if (digitalRead(MIC_0) == LOW) {
           time_dif[1] -= loop_elapse;
         }
-        if (digitalRead(MIC_2) == LOW) {
-          time_dif[0] += loop_elapse;
-        }
         flag_now[1] = true;
       }
 
@@ -31,84 +36,120 @@ void TimeStamp(void) {
         if (digitalRead(MIC_0) == LOW) {
           time_dif[2] -= loop_elapse;
         }
-        if (digitalRead(MIC_1) == LOW) {
-          time_dif[0] -= loop_elapse;
-        }
         flag_now[2] = true;
+      }
+      if (digitalRead(MIC_3) == HIGH) {
+        if (digitalRead(MIC_0) == LOW) {
+          time_dif[3] -= loop_elapse;
+        }
+        flag_now[3] = true;
       }
       delayMicroseconds(3);
     }
   }
 }
 
+
 void GetAngle(void) {
-  if (flag_now[0] == true && flag_now[1] == true && flag_now[2] == true) {
+  if (flag_now[0] == true && flag_now[1] == true && flag_now[2] == true && flag_now[3] == true) {
     delay(1000);
     Serial.print("time_dif[0] = " + String(time_dif[0]) + "\n");
     Serial.print("time_dif[1] = " + String(time_dif[1]) + "\n");
     Serial.print("time_dif[2] = " + String(time_dif[2]) + "\n");
-
-    if ((abs(time_dif[1]) < 500) && (abs(time_dif[2]) < 500) && (time_dif[1] != 0)) {
-      double tangential = (double) (30 * time_dif[2] - 15 * time_dif[1]) / (26 * time_dif[1]);
-      double angle_in_degree = 0;
-
-      if (tangential >= 0) {
-        if (time_dif[2] < 0) {
-          angle = atan(tangential);
-          angle_in_degree = angle * 180 / PI;
-          Serial.println("quadrant = 1");
-        }
-        else {
-          angle = atan(tangential) - PI;
-          angle_in_degree = angle * 180 / PI;
-          Serial.println("quadrant = 3");
-        }
-      }
-
-      if (tangential < 0) { //else 보다 보기편함
-        if (time_dif[1] > 0) {
-          angle = atan(tangential) + PI;
-          angle_in_degree = angle * 180 / PI;
-          Serial.println("quadrant = 2");
-        }
-        else if (time_dif[0] > 0) {
-          angle = atan(tangential);
-          angle_in_degree = angle * 180 / PI;
-          Serial.println("quadrant = 4");
-        }
-      }
-      angle_step = (int) map(angle * 1000, -PI * 1000, PI * 1000, -100, 100);
-      Serial.print("angle = " + String(angle) + "\n");
-      Serial.print("angle_in_degree = " + String(angle_in_degree) + "\n");
-      Serial.print("angle_in_step = " + String(angle_step) + "\n");
-      flag_now[0] = false;
-      flag_now[1] = false;
-      flag_now[2] = false;
-    }
-
-    else {
-      Serial.println("Error! time diffrence is too big or exact 90 dgree");
-    }
-  }
-  else if (!(flag_now[0] == flag_now[1] == flag_now[2] == 0) && (flag_now[0] == false || flag_now[1] == false || flag_now[2] == false)) {
+    Serial.print("time_dif[3] = " + String(time_dif[3]) + "\n");
+    //
+    //    if ((abs(time_dif[1]) < 500) && (abs(time_dif[2]) < 500) && (time_dif[1] != 0)) {
+    //      double tangential = (double) (30 * time_dif[2] - 15 * time_dif[1]) / (26 * time_dif[1]);
+    //      double angle_in_degree = 0;
+    //
+    //      if (tangential >= 0) {
+    //        if (time_dif[2] < 0) {
+    //          angle = atan(tangential);
+    //          angle_in_degree = angle * 180 / PI;
+    //          Serial.println("quadrant = 1");
+    //        }
+    //        else {
+    //          angle = atan(tangential) - PI;
+    //          angle_in_degree = angle * 180 / PI;
+    //          Serial.println("quadrant = 3");
+    //        }
+    //      }
+    //
+    //      if (tangential < 0) { //else 보다 보기편함
+    //        if (time_dif[1] > 0) {
+    //          angle = atan(tangential) + PI;
+    //          angle_in_degree = angle * 180 / PI;
+    //          Serial.println("quadrant = 2");
+    //        }
+    //        else if (time_dif[0] > 0) {
+    //          angle = atan(tangential);
+    //          angle_in_degree = angle * 180 / PI;
+    //          Serial.println("quadrant = 4");
+    //        }
+    //      }
+    //      angle_step = (int) map(angle * 1000, -PI * 1000, PI * 1000, -100, 100);
+    //      Serial.print("angle = " + String(angle) + "\n");
+    //      Serial.print("angle_in_degree = " + String(angle_in_degree) + "\n");
+    //      Serial.print("angle_in_step = " + String(angle_step) + "\n");
     flag_now[0] = false;
     flag_now[1] = false;
     flag_now[2] = false;
+    flag_now[3] = false;
+    //    }
+    //
+    //    else {
+    //      Serial.println("Error! time diffrence is too big or exact 90 dgree");
+    //    }
+  }
+  else if (!(flag_now[0] == flag_now[1] == flag_now[2] == flag_now[3] == 0) && (flag_now[0] == false || flag_now[1] == false || flag_now[2] == false) || flag_now[0] == false) {
+    flag_now[0] = false;
+    flag_now[1] = false;
+    flag_now[2] = false;
+    flag_now[3] = false;
   }
 }
 
-void Move() {
-  double angle_dif = angle_step - last_angle_step;
-  stepper.step(angle_dif);
-  last_angle_step = angle_step;
-  PhaseFree();
-}
+//void Move() {
+//  double angle_dif = angle_step - last_angle_step;
+//  stepper.step(angle_dif);
+//  last_angle_step = angle_step;
+//  PhaseFree();
+//}
 
 void PhaseFree(void) {
-  digitalWrite(MOTER_PIN_1, LOW);
-  digitalWrite(MOTER_PIN_2, LOW);
-  digitalWrite(MOTER_PIN_3, LOW);
-  digitalWrite(MOTER_PIN_4, LOW);
+  digitalWrite(stp, LOW);
+  digitalWrite(dir, LOW);
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+  digitalWrite(EN, HIGH);
+}
+
+void motorctrl(int servo_angle, int angle_step ) {
+  servo.write(servo_angle);
+
+  int angle_dif = angle_step - last_angle_step;
+
+  if (angle_step > 0) {
+    digitalWrite(dir, LOW);
+    for (int x = 0; x <= angle_dif ; x++)
+    {
+      digitalWrite(stp, HIGH);
+      delay(7);
+      digitalWrite(stp, LOW);
+      delay(7);
+    }
+  }
+  else {
+    digitalWrite(dir, HIGH);
+    for (int x = -1; x > angle_dif ; x--)
+    {
+      digitalWrite(stp, HIGH);
+      delay(7);
+      digitalWrite(stp, LOW);
+      delay(7);
+    }
+  }
+  last_angle_step = angle_step;
 }
 
 void Change_Value_in_Serial(void) { //new line
